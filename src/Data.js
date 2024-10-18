850044cb411a12df2687240a3708910fc84ec742
Arpit030 <arpitverma17032003@gmail.com>
2024-10-18 10:47:32 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,46 @@
+const Data = [
+    {
+        id: '1',
+        image: 'https://th.bing.com/th/id/R.7c771ce2cbe3cc55880ed8455118db5e?rik=2JPJLehr8K%2fVGQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2f7%2f7d%2fThe_Taj_Mahal%2cAgra.jpg&ehk=Ia6B806UssgzK79HkKZygUFHuPXEXV%2fDpOK9mu1eUtU%3d&risl=1&pid=ImgRaw&r=0',
+        name: 'Agra',
+        info: 'Agra is a city in the northern state of Uttar Pradesh, India, on the Yamuna River. It is famous for being the capital of the Mughal emperors from 1526 to 1658 and for its many Mughal-era buildings.',
+        price: '35670',
+    },
+    {
+        id: '2',
+        image: 'https://th.bing.com/th/id/OIP.1OaP_aGpqP7BpuFMmzWrUAHaEL?rs=1&pid=ImgDetMain',
+        name: 'Jaipur',
+        info: 'Jaipur is among the most beloved cities in India for first-time travelers and even more seasoned India travel veterans, and it’s easy to see why. Home to impressive palaces, historic homes, colorful temples, multiple forts with impressive views, and even some of India’s best bazaars, Jaipur is a unique and beautiful city that has to be experienced to be believed.',
+        price: '54632',
+    },
+    {
+        id: '3',
+        image: 'https://1.bp.blogspot.com/-H7TJ6sS0TYU/XRiMS6A5-0I/AAAAAAAACRk/zkOJ-vPtPx4vStxWJ3H6WpYpcsCUtON9gCEwYBhgL/s1600/IMG_20190630_151609.jpg',
+        name: 'Vrindavan',
+        info: 'Vrindavan, town in western Uttar Pradesh state, northern India. It is situated on the west bank of the Yamuna River, just north of Mathura. The town is the sacred center of the Hindu deity Krishna and those who worship him. It is especially important to the Gaudiya sect of Vaishnavism and is a major pilgrimage site.',
+        price: '76532',
+    },
+    {
+        id: '4',
+        image: 'https://wallpaperaccess.com/full/4136688.jpg',
+        name: 'Darjiling',
+        info: 'One of the most popular hill stations in India – Darjeeling is a perfect collage of snowy and mighty Himalayan ranges, winding walking tracks, valleys, monasteries, momos, tea plantations, and toy trains. Amidst the green-grey hills, the serene vegetation and scenic vistas guarantee a vacation to cherish forever.',
+        price: '34568',
+    },
+    {
+        id: '5',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMi52z-SEnQcrV4LfZDK6vav223cJaRlXZsw&s',
+        name: 'Goa',
+        info: 'This place is known for its famous beaches, culture, climate, and historical places. You may find colourful architecture, lush greenery, and breathtaking beaches here. Goa is a perfect blend of Portugal and Indian culture. People get attracted to this place to experience world-class music, dance, and sunset.',
+        price: '63783'
+    },
+    {
+        id: '6',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdqNjrfPnDaxxm1cEvcvL2lTLWrwSpklh5Yg&s',
+        name: 'Munnar',
+        info: "Known for its tea plantations, it's the most visited tourist spot in India. Reasons of opting Munnar in Kerala as the most viable tourist spot is, its utility for being the best for Honeymooners, Family vacations, corporate meetings, Trekkers & even for the Art lovers.",
+        price: '34568',
+    },
+];
+
+export default Data;
